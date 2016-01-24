54d039f4d1aca9dad08690f47ccddd66cefb7075
Maxim Fedorenko <varlllog@gmail.com>
2016-01-24 16:31:49 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+// Copyright 2016 Maxim Fedorenko. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"github.com/varlog/go-nano/nano"
+)
+
+func main() {
+	fmt.Println("Hello!")
+	nano.Nano()
+}
---
+++ nano/nano.go
@@ -0,0 +1,13 @@
+// Copyright 2016 Maxim Fedorenko. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package nano
+
+import (
+	"fmt"
+)
+
+func Nano() {
+	fmt.Println("Nano!")
+}
